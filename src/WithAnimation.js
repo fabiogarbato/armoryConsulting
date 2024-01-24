111c7442b7ebcfe6b34bf543e93dfd814c9f7cbd
Fabio <fabio.garbato@mps.com>
2024-01-24 14:22:37 -0300

    Ajuste animação

--- src/WithAnimation.js
+++ src/WithAnimation.js
@@ -9,12 +9,12 @@ const WithAnimation = ({ children, threshold = 0.1 }) => {
     const childrenWithAnimation = React.Children.map(children, child =>
         React.cloneElement(child, {
             className: `${child.props.className} ${animationClass}`,
+            style: { ...child.props.style, opacity: visible ? 1 : 0, transition: 'opacity 1s ease' }
         })
     );
 
     const style = {
         color: visible ? '#ff7300' : 'transparent',
-        transition: 'color 1s ease',
         display: 'flex',  
         justifyContent: 'center',
         alignItems: 'center',
@@ -23,6 +23,7 @@ const WithAnimation = ({ children, threshold = 0.1 }) => {
 
     return <div ref={ref} style={style}>{childrenWithAnimation}</div>;
 };
+
 
 
 export default WithAnimation;
